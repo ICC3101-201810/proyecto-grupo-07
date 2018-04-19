d66412f6a7a6d7b0fe776f5efa357e96b45d3f9c
Jisepulveda <37961857+Jisepulveda@users.noreply.github.com>
2018-04-19 16:59:38 -0300

    Javier 19 abril

---
+++ Proyecto Omega/Proyecto Omega/Cuenta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Omega
+{
+    public class Cuenta
+    {
+        public int id;
+        public string nombre;
+        public string apellido;
+        string nombreUsuario;
+        string claveAcceso;
+        string tipo;
+        string email;
+        List<int> apuntesSubidos;
+        int valoracionCuenta;
+        List<int> cursosRealizados;
+        List<int> amigos;
+        List<int> cuentaFavoritos;
+        string carrera;
+
+        public Cuenta(int miId, string miNombre,string miApellido,string miNombreUsuario,string miClaveAcceso,string miTipo,string miEmail,List<int> miApuntesSubidos,int miValoriacionCuenta,List<int> miCursosRealizados,List<int> miAmigos,List<int> miCuentaFavoritos,string miCarrera)
+        {
+            id = miId;
+            nombre = miNombre;
+            apellido = miApellido;
+            nombreUsuario = miNombreUsuario;
+            claveAcceso = miClaveAcceso;
+            tipo = miTipo;
+            email = miEmail;
+            apuntesSubidos = miApuntesSubidos;
+            valoracionCuenta = miValoriacionCuenta;
+            cursosRealizados = miCursosRealizados;
+            amigos = miAmigos;
+            cuentaFavoritos = miCuentaFavoritos;
+            carrera = miCarrera;
+
+        }
+
+
+    }
+}
---
+++ Proyecto Omega/Proyecto Omega/Sistema.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Omega
+{
+    class Sistema
+    {
+        List<Cuenta> listaCuentas;
+
+        public void crearCuenta(int miId, string miNombre, string miApellido, string miNombreUsuario, string miClaveAcceso, string miEmail, List<int> miApuntesSubidos, int miValoriacionCuenta, List<int> miCursosRealizados, List<int> miAmigos, List<int> miCuentaFavoritos, string miCarrera)
+        {
+            Cuenta nuevaCuenta = new Cuenta(miId, miNombre, miApellido, miNombreUsuario, miClaveAcceso, "Estudiante", miEmail, miApuntesSubidos, miValoriacionCuenta, miCursosRealizados, miAmigos, miCuentaFavoritos, miCarrera);
+            listaCuentas.Add(nuevaCuenta);
+        }
+
+        public void eliminarCuenta(int miId)
+        {
+            foreach (Cuenta revisionCuentas in listaCuentas)
+            {
+                int contador = 0;
+                if (revisionCuentas.id==miId)
+                {
+                    Console.WriteLine("La cuenta de {0} {1} ha sido eliminada satisfactoriamente", revisionCuentas.nombre, revisionCuentas.apellido);
+                    //cuenta[contador]
+                    
+                }
+                contador = contador + 1;
+            }
+            
+        }
+
+    }
+}

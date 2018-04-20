ebc000cd6b69c3b2d445c0fc2b48cc4e397c398b
Pablo Godoy <pigodoy1@miuandes.cl>
2018-04-20 13:38:57 -0400

    Menu de Inicio
    no se por que se habia borrado, subi una copido que tenia en mi pc pero no se si es la mas nueva

---
+++ bin/Proyecto Omega/Proyecto Omega/Menu Inicio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Omega
+{
+    public class Menu_Inicio
+    {
+        public void menuInicial()
+        {
+            while (true)
+            {
+                Console.Write("Bienvenido a tu plataforma de apuntes \n" +
+                  "Si eres miembro inicia sesion o crea una para comenzar a disfrutar del mejor material de estudio\n" +
+                  "1-Inicio Sesion\n" +
+                  "2-Crear Cuenta Nueva\n"
+                  );
+                string opcion = Console.ReadLine();
+                if (opcion == "1")
+                {
+                    Console.Clear();
+                    Console.Write("Ingrese usuario: "); string usuario = Console.ReadLine();
+                    Console.Write("Ingrese contraseña: "); string contrasena = Console.ReadLine();
+
+
+                }
+                
+            }
+        }
+
+        public void inicioSesion()
+        {
+
+        }
+    }
+}

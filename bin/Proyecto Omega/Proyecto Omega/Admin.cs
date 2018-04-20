4c707a44e93a4c1498e6eb774b152e8d1987260a
Pablo Godoy <pigodoy1@miuandes.cl>
2018-04-20 12:28:43 -0400

    Admin Cuenta Actualizados
    mas Metodos

--- bin/Proyecto Omega/Proyecto Omega/Admin.cs
+++ bin/Proyecto Omega/Proyecto Omega/Admin.cs
@@ -8,9 +8,9 @@ namespace Proyecto_Omega
 {
     class Admin : Cuenta
     {
-        public Admin(int RUT ,string Nombre, string Apellido, string NombreUsuario, string ClaveAcceso, 
-                     string Email, string Carrera) 
-             : base(RUT, Nombre, Apellido, NombreUsuario, ClaveAcceso, Email, Carrera){ }
+        public Admin(int RUT, string Nombre, string Apellido, string NombreUsuario, string ClaveAcceso,
+                     string Email, string Carrera)
+             : base(RUT, Nombre, Apellido, NombreUsuario, ClaveAcceso, Email, Carrera) { }
 
         public bool CrearCuenta(Object[] DatosCuenta, List<Cuenta> TodasCuentas)
         {
@@ -33,14 +33,183 @@ namespace Proyecto_Omega
             return false;
         }
 
-        public bool modificarCuenta(int RUTCuenta, Object[] DatosCuenta, List<Cuenta> TodasCuentas)
+        public bool ModificarCuentaOtro(int RUTCuenta, List<string> DatosCuenta,
+                                        List<Cuenta> TodasCuentas)
         {
             List<Cuenta> cuenta = BuscarCuenta(RUTCuenta, TodasCuentas);
             if (cuenta.Count == 1)
             {
-
+                if (DatosCuenta[0] != "")
+                {
+                    cuenta[0].nombre = DatosCuenta[0];
+                }
+                if (DatosCuenta[1] != "")
+                {
+                    cuenta[0].apellido = DatosCuenta[1];
+                }
+                if (DatosCuenta[2] != "")
+                {
+                    cuenta[0].nombreUsuario = DatosCuenta[2];
+                }
+                if (DatosCuenta[3] != "")
+                {
+                    cuenta[0].claveAcceso = DatosCuenta[3];
+                }
+                if (DatosCuenta[4] != "")
+                {
+                    cuenta[0].email = DatosCuenta[4];
+                }
+                if (DatosCuenta[5] != "")
+                {
+                    cuenta[5].carrera = DatosCuenta[5];
+                }
+                return true;
             }
+            return false;
         }
-        
+
+        public bool AgregarCursoCuenta(int RUTCuenta, int IDCurso, List<Cuenta> TodasCuentas)
+        {
+            List<Cuenta> cuenta = BuscarCuenta(RUTCuenta, TodasCuentas);
+            if (cuenta.Count == 1)
+            {
+                if (!cuenta[0].cursosRealizados.Contains(IDCurso))
+                {
+                    cuenta[0].cursosRealizados.Add(IDCurso);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool BorrarApunte(int IDApunte, List<Apunte> TodosApuntes)
+        {
+            List<Apunte> apunte = BuscarApunteTodos(new object[] { IDApunte, "", "" }, TodosApuntes);
+            if (apunte.Count == 1)
+            {
+                TodosApuntes.Remove(apunte[0]);
+                return true;
+            }
+            return false;
+        }
+
+        //Borra la cuenta con sus apuntes
+        public bool BorrarCuenta(int RUTCuenta, List<Cuenta> TodasCuentas, List<Apunte> TodosApuntes)
+        {
+            List<Cuenta> cuenta = BuscarCuenta(RUTCuenta, TodasCuentas);
+            if (cuenta.Count == 1)
+            {
+                for (int i = 0; i < cuenta[0].apuntesSubidos.Count; i++)
+                {
+                    BorrarApunte(cuenta[0].apuntesSubidos[i], TodosApuntes);
+                }
+                TodasCuentas.Remove(cuenta[0]);
+                return true;
+            }
+            return false;
+        }
+
+        public bool ActualizarApunte(int IDApunte, string[] miDatos, List<Apunte> todosApuntes)
+        {
+            List<Apunte> miApunte = BuscarApunteTodos(new Object[] { IDApunte, "", "" }, todosApuntes);
+            if (miApunte.Count == 1)
+            {
+                if (miDatos[0] != "")                      // Se ve si hay datos a actualizar, solo
+                {                                          // se puede actualizar un topico a la vez.
+                    miApunte[0].titulo = miDatos[0];       // Aqui solo se puede cambiar titulo, 
+                                                           // topicos, carrera y contenido
+                }                                          // los otros datos tienen otros metodos.
+                if (miDatos[2] != "")  // Agrega el topico si no lo tiene.
+                {
+                    if (!miApunte[0].topico.Contains(miDatos[2]))
+                    {
+                        miApunte[0].topico.Add(miDatos[2]);
+                    }
+                }
+                if (miDatos[3] != "")  // Borra el topico si lo tiene
+                {
+                    if (miApunte[0].topico.Contains(miDatos[3]))
+                    {
+                        miApunte[0].topico.Remove(miDatos[3]);
+                    }
+                }
+                if (miDatos[4] != "")
+                {
+                    miApunte[0].carrera = miDatos[4];
+                }
+                if (miDatos[5] != "")
+                {
+                    miApunte[0].contenido = miDatos[5];
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public bool CrearCurso(Object[] DatosCurso, List<Curso> TodosCursos)
+        {
+            bool checkNombre = true;
+            for (int i = 0; i < TodosCursos.Count; i++)
+            {
+                if (TodosCursos[i].nombreCurso == (string)DatosCurso[0])
+                {
+                    checkNombre = false;
+                }
+            }
+            if (checkNombre)
+            {
+                TodosCursos.Add(new Curso((string)DatosCurso[0], (string)DatosCurso[1],
+                                          (string)DatosCurso[2], (List<string>)DatosCurso[3]));
+                return true;
+            }
+            return false;
+        }
+
+        public bool EditarCurso(int IDCurso, List<string> DatosCurso, List<Curso> TodosCursos)
+        {
+            List<Curso> curso = BuscarCurso(IDCurso, TodosCursos);
+            if (curso.Count == 1)
+            {
+                if (DatosCurso[0] != "")
+                {
+                    curso[0].nombreCurso = DatosCurso[0];
+                }
+                if (DatosCurso[1] != "")
+                {
+                    curso[0].facultad = DatosCurso[1];
+                }
+                if (DatosCurso[2] != "")
+                {
+                    curso[0].carrera = DatosCurso[2];
+                }
+                if (DatosCurso[3] != "")
+                {
+                    if (!curso[0].topico.Contains(DatosCurso[3]))
+                    {
+                        curso[0].topico.Add(DatosCurso[3]);
+                    }
+                }
+                if (DatosCurso[4] != "")
+                {
+                    if (curso[0].topico.Contains(DatosCurso[4]))
+                    {
+                        curso[0].topico.Remove(DatosCurso[4]);
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public bool BorrarCurso(int IDCurso, List<Curso> TodosCursos)
+        {
+            List<Curso> curso = BuscarCurso(IDCurso, TodosCursos);
+            if (curso.Count == 1)
+            {
+                TodosCursos.Remove(curso[0]);
+                return true;
+            }
+            return false;
+        }
     }
 }
--- bin/Proyecto Omega/Proyecto Omega/Cuenta.cs
+++ bin/Proyecto Omega/Proyecto Omega/Cuenta.cs
@@ -16,9 +16,9 @@ namespace Proyecto_Omega
         public int RUT;
         public List<int> apuntesSubidos = new List<int>() { };    // Lista de los ID de los apuntes
         public List<int> valoracionCuenta = new List<int>() { };  // que ha subido
-        public List<int> cursosRealizados = new List<int>() { };
+        public List<int> cursosRealizados = new List<int>() { };  // ID de cursos
         public List<Cuenta> amigos = new List<Cuenta>() { };
-        public List<Cuenta> cuentaFavoritos = new List<Cuenta>() { };
+        public List<Cuenta> favoritos = new List<Cuenta>() { };
         public string carrera;
         public List<Reporte> reportes = new List<Reporte>() { };
 
@@ -44,6 +44,7 @@ namespace Proyecto_Omega
             return miApunte;
         }
 
+        //Busca solo en los apuntes que ha subido
         public List<Apunte> BuscarApunte(Object[] miDatos, List<Apunte> todosApuntes) 
                                 // Recibe un array [ID, titulo, topico] y la lista de todos los apuntes
         {                       //                 [int, string, string]
@@ -69,53 +70,65 @@ namespace Proyecto_Omega
             return apuntes;
         }
 
+        //Busca entre todos los apuntes
+        public List<Apunte> BuscarApunteTodos(Object[] miDatos, List<Apunte> todosApuntes)
+        {
+            List<Apunte> apuntes = new List<Apunte>() { };
+            for (int i = 0; i < todosApuntes.Count; i++)
+            {                                            
+                if (todosApuntes[i].ID == (int)miDatos[0])
+                {
+                    apuntes.Add(todosApuntes[i]);
+                }
+                else if (todosApuntes[i].titulo == (string)miDatos[1])
+                {
+                    apuntes.Add(todosApuntes[i]);
+                }
+                else if (todosApuntes[i].topico.Contains((string)miDatos[3]))
+                {
+                    apuntes.Add(todosApuntes[i]);
+                }
+            }
+            return apuntes;
+        }
+
         // Busca entre sus apuntes si existe el apunte a cambiar y luego revisa que se quiere cambiar
         public bool ActualizarApunte(int IDApunte, string[] miDatos, List<Apunte> todosApuntes)
         {   // Al buscar por ID a lo mas puede haber un resultado
             List<Apunte> miApunte = BuscarApunte(new Object[] { IDApunte, "", "" }, todosApuntes);
             if (miApunte.Count == 1)
             {
-                if (miDatos[0] != "")                      // Se ve si hay datos a actualizar,
-                {                                          // solo se puede actualizar uno a la vez.
+                if (miDatos[0] != "")                      // Se ve si hay datos a actualizar, solo
+                {                                          // se puede actualizar un topico a la vez.
                     miApunte[0].titulo = miDatos[0];       // Aqui solo se puede cambiar titulo, 
-                    return true;                           // topicos (uno a la vez), carrera y contenido
+                                                           // topicos, carrera y contenido
                 }                                          // los otros datos tienen otros metodos.
-                else if (miDatos[2] != "")  // Agrega el topico si no lo tiene.
+                if (miDatos[2] != "")  // Agrega el topico si no lo tiene.
                 {
                     if (!miApunte[0].topico.Contains(miDatos[2]))
                     {
                         miApunte[0].topico.Add(miDatos[2]);
-                        return true;
                     }
-                    return false;
                 }
-                else if (miDatos[3] != "")  // Borra el topico si lo tiene
+                if (miDatos[3] != "")  // Borra el topico si lo tiene
                 {
                     if (miApunte[0].topico.Contains(miDatos[3]))
                     {
                         miApunte[0].topico.Remove(miDatos[3]);
-                        return true;
                     }
-                    return false;
                 }
-                else if (miDatos[4] != "")
+                if (miDatos[4] != "")
                 {
                     miApunte[0].carrera = miDatos[4];
-                    return true;
                 }
-                else if (miDatos[5] != "")
+                if (miDatos[5] != "")
                 {
                     miApunte[0].contenido = miDatos[5];
-                    return true;
                 }
+                return true;
             }
             return false;
         }
-
-        //public Cuenta ReportarCuenta(Object[] datosBuscar)
-        //{
-        //      // Faltan los metodos relacionados con cuentas
-        //}
 
         public bool PublicarApunte(int IDApunte, bool miEstado, List<Apunte> todosApuntes)
         {
@@ -157,6 +170,123 @@ namespace Proyecto_Omega
             return cuentaEncontrada;  // Las unicas opciones son una lista de largo 0 o 1
         }
 
+        public bool ReportarCuenta(int RUTCuenta, List<Cuenta> TodasCuentas, string RazonReporte)
+        {
+            List<Cuenta> cuenta = BuscarCuenta(RUTCuenta, TodasCuentas);
+            if (cuenta.Count == 1)
+            {
+                cuenta[0].reportes.Add(new Reporte(this, RazonReporte));
+                return true;
+            }
+            return false;
+        }
 
+        public bool ReportarApunte(int IDApunte, List<Apunte> TodosApuntes, string RazonReporte)
+        {
+            List<Apunte> apunte = BuscarApunteTodos(new Object[] { IDApunte, "", "" }, TodosApuntes);
+            if (apunte.Count == 1)
+            {
+                apunte[0].reportes.Add(new Reporte(this, RazonReporte));
+                return true;
+            }
+            return false;
+        }
+
+        public bool AgregarAmigo(int RUTCuenta, List<Cuenta> TodasCuentas)
+        {
+            List<Cuenta> cuenta = BuscarCuenta(RUTCuenta, TodasCuentas);
+            if (cuenta.Count == 1)
+            {
+                if (!amigos.Contains(cuenta[0]))
+                {
+                    amigos.Add(cuenta[0]);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool EliminarAmigo(int RUTCuenta, List<Cuenta> TodasCuentas)
+        {
+            List<Cuenta> cuenta = BuscarCuenta(RUTCuenta, TodasCuentas);
+            if (cuenta.Count == 1)
+            {
+                if (amigos.Contains(cuenta[0]))
+                {
+                    amigos.Remove(cuenta[0]);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool AgregarFavorito(int RUTCuenta, List<Cuenta> TodasCuentas)
+        {
+            List<Cuenta> cuenta = BuscarCuenta(RUTCuenta, TodasCuentas);
+            if (cuenta.Count == 1)
+            {
+                if (!favoritos.Contains(cuenta[0]))
+                {
+                    favoritos.Add(cuenta[0]);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool EliminarFavorito(int RUTCuenta, List<Cuenta> TodasCuentas)
+        {
+            List<Cuenta> cuenta = BuscarCuenta(RUTCuenta, TodasCuentas);
+            if (cuenta.Count == 1)
+            {
+                if (!favoritos.Contains(cuenta[0]))
+                {
+                    favoritos.Add(cuenta[0]);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<Curso> BuscarCurso(int IDCurso, List<Curso> TodosCursos)
+        {
+            List<Curso> curso = new List<Curso>() { };
+            for (int i = 0; i < TodosCursos.Count; i++)
+            {
+                if (TodosCursos[i].ID == IDCurso)
+                {
+                    curso.Add(TodosCursos[i]);
+                }
+            }
+            return curso;
+        }
+
+        public void modificarCuenta(List<string> DatosCuenta)
+        {
+            if (DatosCuenta[0] != "")
+            {
+                nombre = DatosCuenta[0];
+            }
+            if (DatosCuenta[1] != "")
+            {
+                apellido = DatosCuenta[1];
+            }
+            if (DatosCuenta[2] != "")
+            {
+                nombreUsuario = DatosCuenta[2];
+            }
+            if (DatosCuenta[3] != "")
+            {
+                claveAcceso = DatosCuenta[3];
+            }
+            if (DatosCuenta[4] != "")
+            {
+                email = DatosCuenta[4];
+            }
+            if (DatosCuenta[5] != "")
+            {
+                carrera = DatosCuenta[5];
+            }
+        }
     }
 }

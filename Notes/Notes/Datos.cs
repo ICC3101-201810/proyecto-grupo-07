22e62c31c3997e5ee3b4a8a83eff50b11b71e0b2
Pablo Godoy <pigodoy1@miuandes.cl>
2018-05-11 22:07:51 -0400

    reset

---
+++ Notes/Notes/Datos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    public class Datos
+    {
+        public Sistema Sistema = new Sistema();
+
+        public List<Curso> Cursos = new List<Curso>() { };
+        public List<Apunte> Apuntes = new List<Apunte>() { };
+        public List<Cuenta> Cuentas = new List<Cuenta>() { };
+    }
+}

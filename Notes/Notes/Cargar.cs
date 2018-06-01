77bc8a742834f9b53abfbe7f190dbb5924d692ff
Pablo Godoy <pigodoy1@miuandes.cl>
2018-06-01 00:07:53 -0400

    cargar datos

--- Notes/Notes/Cargar.cs
+++ Notes/Notes/Cargar.cs
@@ -16,16 +16,27 @@ namespace Notes
         {
             try
             {
-                using (Stream stream = File.Open("cursos.bin", FileMode.Open))
-                {
-                    BinaryFormatter bin = new BinaryFormatter();
+                string auxdirectorio = Directory.GetCurrentDirectory();
+                string directorio = auxdirectorio.Substring(0, auxdirectorio.Count() - 9);
+                string finalDirectorio = "\\Datos\\Cursos";
+                List<Curso> cursosCargados = new List<Curso>() { };
 
-                    return (List<Curso>)bin.Deserialize(stream);
+                string[] cursos = Directory.GetFiles(finalDirectorio, "*.bin");
+
+                foreach (string IDCurso in cursos)
+                {
+                    using (Stream stream = File.Open(directorio + finalDirectorio + IDCurso, FileMode.Open))
+                    {
+                        BinaryFormatter bin = new BinaryFormatter();
+                        cursosCargados.Add((Curso)bin.Deserialize(stream));
+                    }
+
                 }
+                return cursosCargados;
+                
             }
             catch (IOException)
             {
-                Console.WriteLine("Exception Cargar Cursos");
                 return new List<Curso>() { };
             }
         }
@@ -33,12 +44,24 @@ namespace Notes
         {
             try
             {
-                using (Stream stream = File.Open("apuntes.bin", FileMode.Open))
-                {
-                    BinaryFormatter bin = new BinaryFormatter();
+                string auxdirectorio = Directory.GetCurrentDirectory();
+                string directorio = auxdirectorio.Substring(0, auxdirectorio.Count() - 9);
+                string finalDirectorio = "\\Datos\\Apuntes";
+                List<Apunte> apuntesCargados = new List<Apunte>() { };
 
-                    return (List<Apunte>)bin.Deserialize(stream);
+                string[] apuntes = Directory.GetFiles(finalDirectorio, "*.bin");
+
+                foreach (string IDApunte in apuntes)
+                {
+                    using (Stream stream = File.Open(directorio + finalDirectorio + IDApunte, FileMode.Open))
+                    {
+                        BinaryFormatter bin = new BinaryFormatter();
+                        apuntesCargados.Add((Apunte)bin.Deserialize(stream));
+                    }
+
                 }
+                return apuntesCargados;
+
             }
             catch (IOException)
             {
@@ -49,12 +72,24 @@ namespace Notes
         {
             try
             {
-                using (Stream stream = File.Open("cuentas.bin", FileMode.Open))
-                {
-                    BinaryFormatter bin = new BinaryFormatter();
+                string auxdirectorio = Directory.GetCurrentDirectory();
+                string directorio = auxdirectorio.Substring(0, auxdirectorio.Count() - 9);
+                string finalDirectorio = "\\Datos\\Cuentas";
+                List<Cuenta> cuentasCargadas = new List<Cuenta>() { };
 
-                    return (List<Cuenta>)bin.Deserialize(stream);
+                string[] cuentas = Directory.GetFiles(finalDirectorio, "*.bin");
+
+                foreach (string RUTCuenta in cuentas)
+                {
+                    using (Stream stream = File.Open(directorio + finalDirectorio + RUTCuenta, FileMode.Open))
+                    {
+                        BinaryFormatter bin = new BinaryFormatter();
+                        cuentasCargadas.Add((Cuenta)bin.Deserialize(stream));
+                    }
+
                 }
+                return cuentasCargadas;
+
             }
             catch (IOException)
             {
